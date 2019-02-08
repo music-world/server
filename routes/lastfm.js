31e4480e6282f6df33c510ac5fb9f26a0aab759d
Veneciac <veneciacalista00@gmail.com>
2019-02-08 09:08:11 +0700

    middleware

--- routes/lastfm.js
+++ routes/lastfm.js
@@ -5,5 +5,6 @@ const { verifyUser } = require('../middlewares')
 
 router.get('/toptracks', Controller.getTopTracks)
 router.get('/toptracks/:location', Controller.getCountryTopTracks)
-router.get('/:song', Controller.searchSong)
+router.get('/:song/search', Controller.searchSong)
+router.get('/:artist', Controller.getArtistInfo)
 module.exports = router;
